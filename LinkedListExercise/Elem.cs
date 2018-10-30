2d6dc3c2f106ad31b74d8fdbe7782b0fd41fb2de
Gabriel Khachatryan <gmkhach@gmail.com>
2018-10-30 09:53:01 -0400

    maded the Reverse method into the Elem class to avoid infinit loops when reassigning pointers

--- LinkedListExercise/Elem.cs
+++ LinkedListExercise/Elem.cs
@@ -10,6 +10,17 @@ namespace LinkedListExercise
     {
         public int val { get; set; }
         public Elem next { get; set; }
-    }
 
+        public Elem Reverse()
+        {
+            if (next == null)
+            {
+                return this;
+            }
+            var temp = next.Reverse();
+            next.next = this;
+            this.next = null;
+            return temp;
+        }
+    }
 }
--- LinkedListExercise/Program.cs
+++ LinkedListExercise/Program.cs
@@ -12,20 +12,9 @@ namespace LinkedListExercise
         {
             //Elem ls = new Elem { val = 1, next = new Elem { val = 2, next = new Elem { val = 3, next = null } } };
             Elem ls = new Elem { val = 1, next = new Elem { val = 2, next = null } };
-            Elem lsReversed = ReverseLinkedList(ls);
+            ls.Reverse();
+            Console.WriteLine(ls);
         }
 
-        static Elem ReverseLinkedList(Elem ls)
-        {
-            if (ls.next == null)
-                return new Elem { val = ls.val, next = null };
-            else
-            {
-                var temp = ls.next;
-                temp.next = ls;
-                ls.next = null;
-                return ReverseLinkedList(temp);
-            }
-        }
     }
 }
