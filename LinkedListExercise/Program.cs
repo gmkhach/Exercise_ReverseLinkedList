7030c13c3695b6e6bbf4f6d58507d914c2fc3d76
Gabriel Khachatryan <gmkhach@gmail.com>
2018-10-30 09:38:46 -0400

    brute-force implementation

--- LinkedListExercise/Program.cs
+++ LinkedListExercise/Program.cs
@@ -10,9 +10,22 @@ namespace LinkedListExercise
     {
         static void Main(string[] args)
         {
-            Elem ls = new Elem { val = 1, next = new Elem { val = 2, next = new Elem { val = 3, next = null } } };
+            //Elem ls = new Elem { val = 1, next = new Elem { val = 2, next = new Elem { val = 3, next = null } } };
+            Elem ls = new Elem { val = 1, next = new Elem { val = 2, next = null } };
+            Elem lsReversed = ReverseLinkedList(ls);
+        }
 
-
+        static Elem ReverseLinkedList(Elem ls)
+        {
+            if (ls.next == null)
+                return new Elem { val = ls.val, next = null };
+            else
+            {
+                var temp = ls.next;
+                temp.next = ls;
+                ls.next = null;
+                return ReverseLinkedList(temp);
+            }
         }
     }
 }
