cf39db1e281da155f5a988736be51dc7f0801056
Gabriel Khachatryan <gmkhach@gmail.com>
2018-10-30 09:18:51 -0400

    Reverse method started, but not fully implemented

---
+++ LinkedListExercise/Elem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListExercise
+{
+    class Elem
+    {
+        public int val { get; set; }
+        public Elem next { get; set; }
+    }
+
+}
