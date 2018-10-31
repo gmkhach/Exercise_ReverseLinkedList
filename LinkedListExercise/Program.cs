7cc4dfd51354843286fb385482f6d98b10c1961d
Gabriel Khachatryan <gmkhach@gmail.com>
2018-10-30 20:32:05 -0400

    fixed the bug in main() where ls.Reverse() was not being assigned to ls

--- LinkedListExercise/Program.cs
+++ LinkedListExercise/Program.cs
@@ -11,7 +11,7 @@ namespace LinkedListExercise
         static void Main(string[] args)
         {
             Elem ls = new Elem { val = 1, next = new Elem { val = 2, next = new Elem { val = 3, next = null } } };
-            ls.Reverse();
+            ls = ls.Reverse();
             Console.WriteLine(ls.Print());
         }
 
